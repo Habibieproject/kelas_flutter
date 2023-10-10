e8659eb7c4da467207e918eba769577b2fae4f91
Habibieproject <projecthabibie@gmail.com>
2023-10-10 14:04:30 +0700

    book app

--- lib/project_train/book_app/detail_book.dart
+++ lib/project_train/book_app/detail_book.dart
@@ -6,13 +6,105 @@ class DetailBook extends StatelessWidget {
   final BookModel data;
   @override
   Widget build(BuildContext context) {
+    final sizeHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Row(
+      body: Stack(
         children: [
-          Image.asset(data.gambarBuku),
-          Text(data.namaBuku),
+          Container(
+            height: sizeHeight * 0.3,
+            decoration: const BoxDecoration(
+                color: Color(
+                  0xff171B36,
+                ),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10))),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      InkWell(
+                          onTap: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Icon(Icons.arrow_back,
+                              color: Colors.white)),
+                      const Icon(Icons.menu, color: Colors.white),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 120,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Image.asset(
+                            data.gambarBuku,
+                            height: 120,
+                          ),
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Row(
+                                children: [
+                                  Expanded(
+                                      child: Text(
+                                    data.namaBuku,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white),
+                                  )),
+                                ],
+                              ),
+                              const Row(
+                                children: [
+                                  Expanded(
+                                      child: Text(
+                                    "The psychology of money is the study of our behavior with money. Success with money isn't about knowledge, IQ or how good you are at math. It's about behavior, and everyone is prone to certain behaviors over others.",
+                                    style: TextStyle(
+                                        fontSize: 8, color: Colors.white),
+                                  )),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  starWidget(),
+                                  starWidget(),
+                                  starWidget(),
+                                  starWidget(),
+                                  starWidget(),
+                                  const Text(
+                                    "(5.0)",
+                                    style: TextStyle(color: Colors.white),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
+    );
+  }
+
+  Icon starWidget() {
+    return const Icon(
+      Icons.star,
+      color: Colors.yellow,
     );
   }
 }
